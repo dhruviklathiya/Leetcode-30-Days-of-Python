566263c90ff4463874a38a2ac9ddfbe238f19da3
DHRUVIK LATHIYA <dhruviklathiya9999@gmail.com>
2025-03-18 23:12:59 +0530

    Day 16 Solution

---
+++ Day 16/solution.py
@@ -0,0 +1,19 @@
+def search_insert(nums, target):
+    left, right = 0, len(nums) - 1
+
+    while left <= right:
+        mid = (left + right) // 2
+        if nums[mid] == target:
+            return mid
+        elif nums[mid] < target:
+            left = mid + 1
+        else:
+            right = mid - 1
+
+    return left  # The correct insertion position
+
+# Example usage:
+print(search_insert([1, 3, 5, 6], 5))  # Output: 2
+print(search_insert([1, 3, 5, 6], 2))  # Output: 1
+print(search_insert([1, 3, 5, 6], 7))  # Output: 4
+print(search_insert([1, 3, 5, 6], 0))  # Output: 0
