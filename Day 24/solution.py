131d0bb71a4244b36cde60f40a7c479315656c70
DHRUVIK LATHIYA <dhruviklathiya9999@gmail.com>
2025-03-26 22:04:33 +0530

    Day 24 Solution

---
+++ Day 24/solution.py
@@ -0,0 +1,21 @@
+def first_missing_positive(nums):
+    n = len(nums)
+    
+    for i in range(n):
+        while (
+            1 <= nums[i] <= n and
+            nums[nums[i] - 1] != nums[i]
+        ):
+            correct_index = nums[i] - 1
+            nums[i], nums[correct_index] = nums[correct_index], nums[i]
+
+    for i in range(n):
+        if nums[i] != i + 1:
+            return i + 1
+
+    return n + 1
+
+# Example usage:
+print(first_missing_positive([1, 2, 0]))          # Output: 3
+print(first_missing_positive([3, 4, -1, 1]))      # Output: 2
+print(first_missing_positive([7, 8, 9, 11, 12]))  # Output: 1
